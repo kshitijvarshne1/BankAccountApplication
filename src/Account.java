cb599a7584bd15be54e2b95d29c74f736e0a0a61
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 01:39:15 +0530

    add fields and constructor

--- src/Account.java
+++ src/Account.java
@@ -5,6 +5,24 @@
  *   File: Account.java
  */
 
-public class Account {
+import java.util.ArrayList;
+
+public class Account implements AccountADT{
+    private long accountNumber;
+    private String accountTitle;
+    private String dateCreated;
+    private long balance;
+    private long interestRate;
+    private ArrayList<Transaction> allTransaction;
+
+    public Account(long accountNumber, String accountTitle, String dateCreated, long balance, long interestRate) {
+        this.accountNumber = accountNumber;
+        this.accountTitle = accountTitle;
+        this.dateCreated = dateCreated;
+        this.balance = balance;
+        this.interestRate = interestRate;
+        this.allTransaction= new ArrayList<>();
+    }
+
 }
 
