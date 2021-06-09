38e9dcd44ae051f0ab7e8a250e3811280c27f9d4
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 01:33:54 +0530

    create Transaction.java

---
+++ src/Transaction.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Jun-21
+ *   Time: 1:33 AM
+ *   File: Transaction.java
+ */
+
+public class Transaction {
+}
+
