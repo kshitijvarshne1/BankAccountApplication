bbea75d7d566d8728b493f1cb0bd324e7bd8f0dc
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 01:42:56 +0530

    add fields and constructor

--- src/Transaction.java
+++ src/Transaction.java
@@ -6,5 +6,16 @@
  */
 
 public class Transaction {
+    private long transactionNumber;
+    private String transactionDate;
+    private String transactionType;
+    private long amount;
+
+    public Transaction(long transactionNumber, String transactionDate, String transactionType, long amount) {
+        this.transactionNumber = transactionNumber;
+        this.transactionDate = transactionDate;
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
 }
 
