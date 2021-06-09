31c30e36d1acf704477cec15618de5fc65ed5983
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 01:30:35 +0530

    create interface AccountADT.java

---
+++ src/AccountADT.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Jun-21
+ *   Time: 1:30 AM
+ *   File: AccountADT.java
+ */
+
+public interface AccountADT {
+}
+
