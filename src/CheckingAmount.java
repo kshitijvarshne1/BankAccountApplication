df9ae0729427966ad98079349ccb03ab238b5795
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 01:32:08 +0530

    craete CheckingAmount.java

---
+++ src/CheckingAmount.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Jun-21
+ *   Time: 1:31 AM
+ *   File: CheckingAmount.java
+ */
+
+public class CheckingAmount {
+}
+
