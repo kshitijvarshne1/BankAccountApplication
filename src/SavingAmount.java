07870c0d79dc482a6632ead3c7f0b879c85452ec
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 01:33:09 +0530

    create SavingAmount.java

---
+++ src/SavingAmount.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Jun-21
+ *   Time: 1:32 AM
+ *   File: SavingAmount.java
+ */
+
+public class SavingAmount {
+}
+
