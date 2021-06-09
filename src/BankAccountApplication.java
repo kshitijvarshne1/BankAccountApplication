9b261bfacbc985d89d6e0f3f276445d1a99f3daa
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 01:55:22 +0530

    create BankAccountApplication.java

---
+++ src/BankAccountApplication.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Jun-21
+ *   Time: 1:55 AM
+ *   File: BankAccountApplication.java
+ */
+
+public class BankAccountApplication {
+}
+
